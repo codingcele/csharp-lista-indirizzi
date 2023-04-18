e262522315890d67c8a63d6a86a9ad9a0e05229c
codingcele <marcocelegato9@gmail.com>
2023-04-18 11:57:39 +0200

    scrittura dati utenti su console

---
+++ Indirizzo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    public class Indirizzo
+    {
+
+        public string? Nome { get; set; }
+        public string? Cognome { get; set; }
+        public string? Via { get; set; }
+        public string? Citta { get; set;}
+        public string? Provincia { get; set; }
+        public string? Zip { get; set; }
+
+        public Indirizzo(string nome, string cognome, string via, string citta, string provincia, string zip)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Via = via;
+            Citta = citta;
+            Provincia = provincia;
+            Zip = zip;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Files;
+
+List<Indirizzo> indirizzi = new List<Indirizzo>();
+
+if (!File.Exists("indirizzi.txt"))
+    Console.WriteLine("File non trovato!");
+else
+{
+    StreamReader sr = File.OpenText("indirizzi.txt");
+
+    string? firstLine = sr.ReadLine();
+
+    while (!sr.EndOfStream)
+    {
+        string? line = sr.ReadLine();
+        if (line != null)
+        {
+            string[] campi = line.Split(',');
+            if (campi.Length >= 6)
+            {
+                Console.WriteLine("UTENTE:");
+                Console.WriteLine($"\tNome: {campi[0]}");
+                Console.WriteLine($"\tCognome: {campi[1]}");
+                Console.WriteLine($"\tVia: {campi[2]}");
+                Console.WriteLine($"\tCittà: {campi[3]}");
+                Console.WriteLine($"\tProvincia: {campi[4]}");
+                Console.WriteLine($"\tZip: {campi[5]}");
+                //indirizzi.Add(new Indirizzo(campi[0], campi[1], campi[2], campi[3], campi[4], campi[5]));
+            }
+        }
+    }
+    sr.Close();
+}
